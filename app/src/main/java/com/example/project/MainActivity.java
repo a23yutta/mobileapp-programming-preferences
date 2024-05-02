b36e2ebda601dc95125eee483627159286d36574
frezzyneel <rileypringles2.0@gmail.com>
2024-05-02 15:02:00 +0200

    Shared preference implemented, cannot set preference reference name

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.project;
 
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences myPreferenceRef;
+    SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
                 Log.d("Tag", "SecondActivity");
             }
         });
+    }
+    public void onResume() {
+        super.onResume();
+
+        myPreferenceRef = getSharedPreferences("myPreferenceString", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        TextView textView = new TextView(this);
+        textView=(TextView)findViewById(R.id.myTextView);
+        textView.setText(myPreferenceRef.getString("myPreferenceString", "No preference found."));
+        Log.d("Tag", "update on resume");
+
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,26 +1,45 @@
 package com.example.project;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class SecondActivity extends AppCompatActivity {
+    private  EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        editText = new EditText(this);
+        editText = (EditText) findViewById(R.id.storedEditedText);
 
         Button closeSecondActivityButton = findViewById(R.id.closeSecondActivity);
         closeSecondActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Log.d("Tag", "Close SecondActivity");
                 finish();
+                Log.d("Tag", "Close SecondActivity");
+
             }
         });
 
+        Button saveEditedTextButton = findViewById(R.id.saveButton);
+        saveEditedTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences myPreferenceRef = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
+                myPreferenceEditor.putString("myPreferenceString", editText.getText().toString());
+                myPreferenceEditor.apply();
+                Log.d("Tag", "Text saved" + editText.getText().toString());
+            }
+        });
     }
+
 }
